8a0c51f6256cb9c0c091f70bbf0385121fc19833
Juanan557 <juanan.galangonzalez@gmail.com>
2018-07-01 19:03:30 +0200

    rosy: Add Media configs
     * clean omx packages

--- device.mk
+++ device.mk
@@ -128,6 +128,11 @@ PRODUCT_COPY_FILES += \
 
 # Media 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
@@ -143,9 +148,9 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw \
-    libstagefright_soft_flacdec
-
+    libstagefright_soft_flacdec \
+    libstagefrighthw
+    
 # Power
 PRODUCT_PACKAGES += \
     power.msm8953
